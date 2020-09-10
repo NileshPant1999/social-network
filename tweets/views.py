98b8db5c83b46e68419b50d6dee7281afc2f174d
Nilesh Pant <nileshpant6969@gmail.com>
2020-09-11 00:07:24 +0530

    like buttons

--- tweets/views.py
+++ tweets/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
-
+import random
 from .models import Tweet
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Hello World </h1>")
     return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qx = Tweet.objects.all()
+    tweet_list = [{"id":x.id, "content":x.content, "likes": random.randint(0,100)} for x in qx]
+
+    data = {
+        "isUser": False,
+        "response":tweet_list
+    }
+
+    return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
